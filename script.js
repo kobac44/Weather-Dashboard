3b9b1addf5ba746a59de0d2d570e4d80d73b81a6
Unko <kobie.watkins@gmail.com>
2020-06-25 01:03:04 -0400

    committed some js outlines

---
+++ script.js
@@ -0,0 +1,27 @@
+let cities = [
+  "Maine",
+  "New York",
+  "North Carolina",
+  "Nashville",
+  "Chicago",
+  "Rexburg",
+  "Hawaii",
+];
+let apikey = "";
+let lat = "latitude";
+let lon = "longitude";
+let uvIndex = lat + lon;
+
+//forEach for the cities function
+
+//get geolocation
+
+// search for a city, will need a local storage
+
+//will need an api key for currentForecast
+
+//click on cities in the list group and see the current temperature
+
+//in the currentForecast container, temperature, humdity, wind speed and UV index
+
+//5day forecast in the border boxes.
