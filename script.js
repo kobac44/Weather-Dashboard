6ccc962ae00f018a8862f66652a9c8e1faa9db17
Unko <kobie.watkins@gmail.com>
2020-06-25 15:37:23 -0400

    added more js code, so far minimal errors corrected, geolocation coded using openweather.org, put in the code to use icons...no icons visible yet

--- script.js
+++ script.js
@@ -7,14 +7,54 @@ let cities = [
   "Rexburg",
   "Hawaii",
 ];
-let apikey = "";
+let url = "";
+letqueryurl = "";
+var apiKey = "bc446cf7ce1e7b9894b233c0b69caad0";
 let lat = "latitude";
 let lon = "longitude";
 let uvIndex = lat + lon;
 
-//forEach for the cities function
-
 //get geolocation
+//https:api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&
+//exclude={part}&appid={YOUR API KEY}
+
+function currentWeather() {
+  navigator.geolocation.getCurrentPosition(function (position) {
+    longitude = position.coords.longitude;
+    latitude = position.coords.latitude;
+
+    var queryURL =
+      "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&appid=" +
+      apiKey;
+
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    })
+      //hard part=//store all of the retrieved data inside of an object called "response" (icons)
+      .then(function (response) {
+        var iconCode = response.wether[0].icon;
+
+        //get icon to response from open-weather-map
+        var iconurl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+        $(".city").html("<li>" + response.name + "</li>");
+        $(".temp").text(
+          "Temperature:" +
+            ((response.main.temp - 273.15) * 1.8 + 32).toFixed(0) +
+            "°F"
+        );
+        $(".humidity").text("Humidity:" + response.main.humidity + "%");
+        $(".wind").text("Wind Speed:" + response.wind.speed + "MPH");
+        $("#wicon").attr("src", iconurl);
+      });
+  });
+}
+
+//forEach for the cities function
 
 // search for a city, will need a local storage
 
